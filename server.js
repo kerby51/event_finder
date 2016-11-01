b3d1b9541e68f6fa25bfc4aea671325387debfd8
katharine benefico <kbenefico@gmail.com>
2016-10-31 20:08:19 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+if (!process.env.AUTH_TOKEN) {
+    require('dotenv').config();
+}
+
+if (!process.env.APP_KEY) {
+    require('dotenv').config();
+}
+
+const path = require('path');
+const app = require('./app/app');
+const express = require('express');
+
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+
+const compiler = webpack(config);
+const webpackMiddle = webpackDevMiddleware(compiler, {
+  publicPath: '/js',
+  stats: {
+    chunks: false,
+    colors: true,
+  },
+});
+
+app.use(webpackMiddle);
+app.use(webpackHotMiddleware(compiler));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on Port 3000`);
+});
