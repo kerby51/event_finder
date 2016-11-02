e9178d81c9fbdfa2fa0a0bf6b58b653374738c72
katharine benefico <kbenefico@gmail.com>
2016-11-02 11:45:53 -0400

    initial commit

--- app/app.js
+++ app/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const authRouter = require('./routes/authRouter');
+const userRouter = require('./routes/userRouter');
+const authentication = require('./middleware/authentication');
+const session = require('express-session')
 const EventKeywords = require('./Eventful/EventKeywords');
 const EventLocation = require('./Eventful/EventLocation');
 
@@ -7,11 +13,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-const morgan = require('morgan');
 app.use(morgan('dev'));
 
-app.get('/api/events/:keywords', (request, response) => {
+app.get('/api/keyword-events/:keywords', (request, response) => {
   const eventBot = new EventKeywords();
   eventBot.getEventsByKeywords(request.params.keywords).then((eventData) => {
     // console.log('eventdata', eventData)
@@ -19,11 +25,30 @@ app.get('/api/events/:keywords', (request, response) => {
   });
 });
 
-app.get('/api/events/searchby/:location', (request, response) => {
+app.get('/api/location-events/:location', (request, response) => {
   const eventBotLocation = new EventLocation();
   eventBotLocation.getEventsByLocation(request.params.location).then((eventDataLocation) => {
     response.status(200).send(eventDataLocation);
   });
 });
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: true,
+  saveUninitialized: true,
+}));
+
+
+app.use('/api', authentication);
+app.use('/api', authRouter);
+app.use('/api/users', userRouter);
+// app.use('/api/events', eventRouter);
+
 module.exports = app;
+
+
+
+
+
+
