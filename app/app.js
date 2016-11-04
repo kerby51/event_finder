87e662b4b1c28f6f06fc845aa9bf4fd39f935e59
katharine benefico <kbenefico@gmail.com>
2016-11-04 14:33:37 -0400

    allow app to use eventRouter

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const authRouter = require('./routes/authRouter');
 const userRouter = require('./routes/userRouter');
+const eventRouter = require('./routes/eventRouter');
 const authentication = require('./middleware/authentication');
 const session = require('express-session')
 const EventKeywords = require('./Eventful/EventKeywords');
@@ -17,7 +18,7 @@ app.use(cookieParser());
 
 app.use(morgan('dev'));
 
-app.get('/api/keyword-events/:keywords', (request, response) => {
+app.get('/api/keyword-events/:keywords/', (request, response) => {
   const eventBot = new EventKeywords();
   eventBot.getEventsByKeywords(request.params.keywords).then((eventData) => {
     // console.log('eventdata', eventData)
@@ -25,9 +26,9 @@ app.get('/api/keyword-events/:keywords', (request, response) => {
   });
 });
 
-app.get('/api/location-events/:location', (request, response) => {
+app.get('/api/location-events/:location/:when', (request, response) => {
   const eventBotLocation = new EventLocation();
-  eventBotLocation.getEventsByLocation(request.params.location).then((eventDataLocation) => {
+  eventBotLocation.getEventsByLocation(request.params.location, request.params.when).then((eventDataLocation) => {
     response.status(200).send(eventDataLocation);
   });
 });
@@ -43,7 +44,7 @@ app.use(session({
 app.use('/api', authentication);
 app.use('/api', authRouter);
 app.use('/api/users', userRouter);
-// app.use('/api/events', eventRouter);
+app.use('/api/events', eventRouter);
 
 module.exports = app;
 
