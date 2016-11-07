99feb1c84d4be8bc214bc3f5e2fcd94141bc0c55
katharine benefico <kbenefico@gmail.com>
2016-11-06 19:56:08 -0500

    testing out routes

--- client/index.jsx
+++ client/index.jsx
@@ -2,18 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './components/App.jsx';
-// import MyEvents from './components/events/MyEvents.jsx';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-
-
-// ReactDOM.render((
-//   <Router history={hashHistory} >
-//     <Route path="/" component={App} >
-//       <Route path="events" component={MyEvents} />
-//     </Route>
-//   </Router>
-//   ), document.querySelector('#root'));
 
 console.log('working');
 
